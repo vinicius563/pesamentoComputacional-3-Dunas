25c4b6bc05a55ee79574c62a0efc575a8bb64ea8
JOSE VINICIUS MARCELINO DE FREITAS <202408155654@alunos.unifanor.edu.br>
2025-05-27 20:05:13 -0300

    aula 27/05

---
+++ condicional.py
@@ -0,0 +1,15 @@
+# Condicionais em Python
+# Condicionais são estruturas que permitem executar diferentes blocos de código com base em condições.
+# Estruturas condicionais
+# if = se
+# else = senão
+# elif = se não, se
+# Exemplo de uso de condicionais
+# Verificando a idade de uma pessoa
+idade = 22
+if idade == 18:
+    print("Você é maior de idade")     
+elif idade < 18:
+    print("Você é menor de idade")
+else:
+    print("Você é maior de idade, mas não tem 18 anos")
---
+++ loopfor.py
@@ -0,0 +1,6 @@
+# Loop for em Python
+# O loop for é usado para iterar sobre uma sequência (como uma lista, tupla ou string) ou um objeto iterável.
+# Exemplo de uso do loop for
+# Iterando com range
+for i in range(10):
+    print(i)    
